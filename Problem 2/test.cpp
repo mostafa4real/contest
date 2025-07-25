e91e9fed665cb5b7ae31ebc33ed44337615a0dda
mostafa4real <noflm9959@gmail.com>
2025-07-25 23:39:24 +0300

    some solutions of some problems

---
+++ Problem 2/test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a;
+   long long C;
+   char g;
+   double b;
+   float f;
+
+   cin>>a>>C>>g>>b>>f;
+   
+   cout<<a<<"\n";
+   cout<<C<<"\n";
+   cout<<g<<"\n";
+   cout<<b<<"\n";
+   cout<<f<<"\n";
+   
+
+    return 0;
+}
